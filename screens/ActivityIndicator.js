cc9bb07660a6a1896f5b55c4ced7fd5462771d41
gauravthorath <gauravjobs25@gmail.com>
2021-01-25 00:42:21 +0530

    Added Activity Indicator component

---
+++ screens/ActivityIndicator.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+const ActivityIndicators = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#ececec"
+    
+  },
+  horizontal: {
+    flexDirection: "Column",
+    justifyContent: "space-evenly",
+    padding: 10,
+    height: "90%"
+  }
+});
+
+export default ActivityIndicators;
