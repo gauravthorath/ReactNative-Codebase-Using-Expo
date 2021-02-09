6e780befbc5ebc202ad60fe888b7b954562db60e
gauravthorath <gauravjobs25@gmail.com>
2021-02-09 13:56:21 +0530

    Updated TextInput component

--- screens/TextInput.tsx
+++ screens/TextInput.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, StyleSheet } from 'react-native';
 
 const UselessTextInput = () => {
   const [value, onChangeText] = React.useState('Useless Placeholder');
 
   return (
     <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      style={ styles.TextInput }
       onChangeText={text => onChangeText(text)}
       value={value}
     />
   );
 }
+const styles = StyleSheet.create({
+TextInput : {
+  height:40,
+  borderColor: 'gray',
+  borderWidth: 1,
+  flex:1,
+  justifyContent:'center',
+
+}
+
+});
 
 export default UselessTextInput;
