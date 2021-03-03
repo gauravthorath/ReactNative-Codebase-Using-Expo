ede287c2a172ee18178d55e6784f4a69acfca71c
gauravthorath <gauravjobs25@gmail.com>
2021-03-04 00:18:42 +0530

    Corrected errors of Title passed in to the Item component

--- screens/VirtualizedList.tsx
+++ screens/VirtualizedList.tsx
@@ -5,18 +5,20 @@ const DATA: never[] = [];
 
 const getItem = (data: any, index: number) => ({
   id: Math.random().toString(12).substring(0),
-  title: `Item ${index+1}`
+  title: `Item ${index+1}`,
+  key: Math.random().toString(12).substring(0)
 });
 
 const getItemCount = (data: any) => 50;
 
-const Item = ({ title }) => {
+const Item = ({title}:{title:string}) => {
     return (
         <View style={styles.item}>
             <Text style={styles.title}>{title}</Text>
         </View>
     );
 };
+
 
 const VirtualiedList = () => {
   return (
